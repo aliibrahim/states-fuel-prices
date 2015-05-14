5b918eecdccb2d83e481da23af3505cfd5f97c1c
Ali Ibrahim <ali.ibrahim@locafox.de>
2015-05-14 14:32:27 +0200

    Added views for showing prices form

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -2,9 +2,18 @@ require 'tasks/fuel_gauge_scraper'
 
 class PricesController < ApplicationController
 
+  include ActionController::MimeResponds #For returning html formats By default rails-api provides only json responses
+  include ActionView::Layouts # For rendering layouts
+
   def index
     @prices = FuelGaugeScraper.fuel_data(params[:state])
     return render json: "Not Found", status: 400 unless @prices.present?
     render json: @prices
+  end
+
+  def states
+    respond_to do |format|
+      format.html {render layout: "prices"}
+    end
   end
 end
